23916a3d7ee50fd55beec0841d563dc140612057
Airat-vagapov <airlake@yandex.ru>
2020-10-15 22:28:14 +0300

    fixed #1

--- src/CashbackService.java
+++ src/CashbackService.java
@@ -1,23 +1,21 @@
 public class CashbackService {
     double cashbackCalculate(
-//          Расходы по категориям
+
             double expensesRegular,
             double expensesIncreased,
             double expensesSpecialOffer,
-//          Проценты кэшбека по категориям
+
             double cashbackPercentRegular,
             double cashbackPercentIncreased,
             double cashbackPercentSpecialOffer
-            ) {
-//      Расчет суммы кэшбека по каждой категории
+    ) {
+
         double cashbackRegular = expensesRegular * cashbackPercentRegular / 100;
         double cashbackIncreased = expensesIncreased * cashbackPercentIncreased / 100;
-        double cashbackSpecialOffer = expensesSpecialOffer * cashbackPercentSpecialOffer /100;
+        double cashbackSpecialOffer = expensesSpecialOffer * cashbackPercentSpecialOffer / 100;
 
-//      Расчет общей суммы кэшбека
         double cashbackSum = cashbackRegular + cashbackIncreased + cashbackSpecialOffer;
 
-//      Условие по ограничению суммы кэшбека
         double limit = 3_000.00;
         if (cashbackSum > limit) {
             cashbackSum = limit;
